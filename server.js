950b414d74f2c3d847eb56ef1c4795306a23bd49
Afton Lawver <lawverap25@gmail.com>
2023-11-09 11:57:50 -0600

    Set up connection to mongodb from server.

--- server.js
+++ server.js
@@ -14,6 +14,26 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/html')));
 
+mongoose.connect(process.env.DB_URI);
+const db = mongoose.connection;
+
+db.on('error', ()=> {console.log("Error connecting to Database.")});
+db.once('open', ()=> {console.log("Connected to Database.")});
+app.post('/submit', (req, res) => {
+    const data = req.body;
+
+    db.collection('inquiries').insertOne(data, (err, collection) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            console.log("Record inserted successfully.");
+        }
+    });
+    return res.redirect('form_submission_success.html');
+})
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
 })
